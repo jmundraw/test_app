46662e2ae35560c12a2b993947b406103c3318dd
Jackie Mundrawala <jmundrawala@gmail.com>
2015-12-06 17:10:50 +0000

    Create new page form and add link to index to add page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
 
 	def index
+		@pages = Page.all
+	end
+
+	def new
+		@page = Page.new
+	end
+
+	def show
+		@page = Page.find(params[:id])
+	end
+
+	def create
+		@page = Page.create(page_params)
+		redirect_to page_path(@page.id)
+	end
+
+	private
+
+	def page_params
+		params.require(:page).permit(:name, :category, :author, :content)
 	end
 end
